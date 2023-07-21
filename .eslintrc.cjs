f4c0e11f2fab22e6c671888a63eb12e604c682e4
Logicer <81077770+Logicer16@users.noreply.github.com>
2023-07-21 10:53:31 +0000

    temporarily remove self as dependency

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,22 @@
 module.exports = {
-  extends: ["plugin:@logicer/recommended-prettier"],
-  plugins: ["@logicer"],
+  // extends: ["plugin:@logicer/recommended-prettier"],
+  // plugins: ["@logicer"],
   root: true,
 
   overrides: [
     {
       files: ["src/**/*"],
       extends: [
-        "plugin:@logicer/recommended-typescript",
+        // "plugin:@logicer/recommended-typescript",
         "plugin:eslint-plugin/recommended",
-        "plugin:@logicer/recommended-jsdoc",
-        "plugin:@logicer/recommended-prettier"
+        // "plugin:@logicer/recommended-jsdoc",
+        // "plugin:@logicer/recommended-prettier"
       ],
       parser: "@typescript-eslint/parser",
       parserOptions: {
         project: "./tsconfig.json"
       },
-      plugins: ["@logicer"]
+      // plugins: ["@logicer"]
     }
   ]
 };
