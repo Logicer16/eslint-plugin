a23f89e8017ca89d1a2656fe56493799fb124337
Logicer <logicer@logicer.cc>
2023-12-07 06:38:43 +0000

    fix name of deprecation config

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@
  */
 
 import {name as packageName, version as packageVersion} from "../package.json";
-import deprecated from "./configs/deprecated.js";
+import deprecation from "./configs/deprecation.js";
 import jsdocConfig from "./configs/jsdoc.js";
 import prettierConfig from "./configs/prettier.js";
 import recommendedConfig from "./configs/recommended.js";
@@ -12,7 +12,7 @@ import typescriptConfig from "./configs/typescript.js";
 
 export = {
   configs: {
-    "deprecated": deprecated,
+    "deprecation": deprecation,
     "recommended": recommendedConfig,
     "recommended-jsdoc": jsdocConfig,
     "recommended-prettier": prettierConfig,
