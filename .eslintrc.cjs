66eff3c1b1202298dc6b0a35e042248e6e16095d
Logicer <logicer@logicer.cc>
2023-12-07 06:29:20 +0000

    use deprecation config as dogfooding

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,7 @@ module.exports = {
         "plugin:@logicer/recommended-typescript",
         "plugin:eslint-plugin/recommended",
         "plugin:@logicer/recommended-jsdoc",
+        "plugin:@logicer/deprecated",
         "plugin:@logicer/recommended-prettier"
       ],
       parser: "@typescript-eslint/parser",
