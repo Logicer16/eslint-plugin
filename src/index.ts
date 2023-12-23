c94da98e1f58c6d17cfabf542c6657f589dc7cbc
Logicer <logicer@logicer.cc>
2023-12-23 12:20:23 +0000

    bump to v3
    New required plugins in existing configs

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@
  */
 
 import {name as packageName, version as packageVersion} from "../package.json";
-import deprecation from "./configs/deprecation.js";
 import jsdocConfig from "./configs/jsdoc.js";
 import prettierConfig from "./configs/prettier.js";
 import recommendedConfig from "./configs/recommended.js";
@@ -12,8 +11,6 @@ import typescriptConfig from "./configs/typescript.js";
 
 export = {
   configs: {
-    // eslint-disable-next-line deprecation/deprecation
-    "deprecation": deprecation,
     "recommended": recommendedConfig,
     "recommended-jsdoc": jsdocConfig,
     "recommended-prettier": prettierConfig,
